4fe6e29f39d5143a04c18978714cd3f2f8d630b8
Domen16 <domen.zupancic6@gmail.com>
2023-03-28 13:09:18 +0200

    Initial layouts -> browsing,creat job ad, missing user profile

---
+++ app/src/main/java/com/example/ucvme/BrowseJobs.java
@@ -0,0 +1,17 @@
+package com.example.ucvme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BrowseJobs extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.browse_jobs);
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/ucvme/BrowseProfiles.java
@@ -0,0 +1,14 @@
+package com.example.ucvme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BrowseProfiles extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.browse_profiles);
+
+
+
+    }
+}
