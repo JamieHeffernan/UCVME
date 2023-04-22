d678a1de4d89df212ee475742cff0360cd9fbad4
karla8marta <95685389+karla8marta@users.noreply.github.com>
2023-04-22 17:50:17 +0200

    added calendar

--- app/src/main/java/com/example/ucvme/CreateJob.java
+++ app/src/main/java/com/example/ucvme/CreateJob.java
@@ -1,26 +1,33 @@
 package com.example.ucvme;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
-
+import java.util.Calendar;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateJob extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
     private ActionBar actionBar;
     private DatabaseReference mDatabase;
+    FirebaseAuth auth;
+    FirebaseUser user;
     Button browseJobs, addJobs;
     EditText addCounty, addLocation, addDescription, addTitle, addWage;
     Spinner addJobType;
@@ -30,6 +37,16 @@ public class CreateJob extends AppCompatActivity implements AdapterView.OnItemSe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_job);
 
+        // Get current user
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        // Initialise calendar elements
+        initDatePicker();
+        dateButton = findViewById(R.id.datePickerButton);
+        dateButton.setText(getTodaysDate());
+
+        // Get instance of DatabaseReference
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
         // Initialise UI elements
@@ -51,17 +68,14 @@ public class CreateJob extends AppCompatActivity implements AdapterView.OnItemSe
         addJobType.setAdapter(adapter);
         addJobType.setOnItemSelectedListener(this);
 
-
         // initialise actionbar
         actionBar = getSupportActionBar();
         // title
         actionBar.setTitle("Create Job");
 
-
         // Button to go to browse jobs
         switchBrowseJobs = new Intent(CreateJob.this,
                 BrowseJobs.class);
-
         browseJobs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -79,9 +93,57 @@ public class CreateJob extends AppCompatActivity implements AdapterView.OnItemSe
 
     }
 
+    // Get current date
+    private String getTodaysDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    // Initialise Date Picker
+    private void initDatePicker()
+    {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+
+    }
+
+    // Format Date
+    private String makeDateString(int day, int month, int year)
+    {
+        return day + "/" + month + "/" + year;
+    }
+
+
+    public void openDatePicker(View view)
+    {
+        datePickerDialog.show();
+    }
+
     // Add job to Database
     public void writeNewJob() {
-        // timestamp
+        // Create id = timestamp + jobLocation
         String timestamp = addLocation.getText().toString() + System.currentTimeMillis();
 
         Job job = new Job(addTitle.getText().toString(),
@@ -90,20 +152,21 @@ public class CreateJob extends AppCompatActivity implements AdapterView.OnItemSe
                 addLocation.getText().toString(),
                 addJobType.getSelectedItem().toString(),
                 addDescription.getText().toString(),
+                dateButton.getText().toString(),
+                user.getEmail(),
                 timestamp);
 
         mDatabase.child("jobs").child(timestamp).setValue(job);
     }
 
+    // Retrieve selected item
     public void onItemSelected(AdapterView<?> parent, View view,
                                int pos, long id) {
-        // An item was selected. You can retrieve the selected item using
          parent.getItemAtPosition(pos);
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
         // Another interface callback
     }
-
 
 }
--- app/src/main/java/com/example/ucvme/Job.java
+++ app/src/main/java/com/example/ucvme/Job.java
@@ -2,21 +2,30 @@ package com.example.ucvme;
 
 public class Job {
 
-    private String title, wage, county, location, jobType, description, jobID;
+    private String title, wage, county, location, jobType, description, startDate, postedBy, jobID;
 
-    public Job(String title, String wage, String county, String location, String jobType, String description, String jobID) {
+    public Job(String title, String wage, String county, String location, String jobType, String description, String startDate, String postedBy, String jobID) {
         this.title = title;
         this.wage = wage;
         this.county = county;
         this.location = location;
         this.jobType = jobType;
         this.description = description;
+        this.startDate = startDate;
+        this.postedBy = postedBy;
         this.jobID = jobID;
+    }
+
+    public String getPostedBy() {
+        return postedBy;
     }
 
     public Job(String title, String wage, String county, String location, String jobType, String timestamp) {
     }
 
+    public String getStartDate() {
+        return startDate;
+    }
 
     public String getCounty() {
         return county;
